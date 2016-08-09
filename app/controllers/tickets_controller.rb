af5412bfe607dcb1b1306ca4aa6f7e8c6edda85d
LenaU <shimetoya@gmail.com>
2016-08-09 13:36:47 +0300

    commit task 14 (new branch) fix bugs

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -43,6 +43,6 @@ class TicketsController < ApplicationController
   end
 
   def ticket_params
-    params.require(:ticket).permit(:fio, :first_station_id, :last_station_id)
+    params.require(:ticket).permit(:fio, :first_station_id, :last_station_id, :train_id)
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,9 @@
 class Ticket < ActiveRecord::Base
   validates :fio, presence: true
+  validates :first_station, presence: true
+  validates :last_station, presence: true
+  validates :train, presence: true
+
   belongs_to :user
   belongs_to :train
   belongs_to :first_station, class_name: 'Station', foreign_key: :first_station_id
